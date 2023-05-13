2a6aff3960de78397009de292caa6aeb14f1020a
Mufaddal5253110 <mufaddalshakir55@gmail.com>
2023-05-13 20:03:34 +0530

    login controller and route added

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -3,6 +3,41 @@ import express from 'express';
 import { getUserByEmail, createUser } from '../db/users';
 import { authentication, random } from '../helpers';
 
+export const login = async (req: express.Request, res: express.Response) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.sendStatus(400);
+    }
+
+    const existingUser = await getUserByEmail(email).select('+authentication.salt +authentication.password');
+  
+    if (!existingUser) {
+      return res.sendStatus(400);
+    }
+
+    const expectedHash = authentication(existingUser.authentication!.salt!, password);
+
+    if (existingUser.authentication!.password != expectedHash) {
+      return res.sendStatus(403);
+    }
+
+    const salt = random();
+    existingUser.authentication!.sessionToken = authentication(salt, existingUser._id.toString());
+
+    await existingUser.save();
+
+    res.cookie('MUFADDAL-AUTH', existingUser.authentication!.sessionToken, { domain: 'localhost', path: '/' });
+
+
+    return res.status(200).json(existingUser).end();
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+}
+
 export const register = async (req: express.Request, res: express.Response) => {
   try {
     const { email, password, username } = req.body;
