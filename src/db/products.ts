c5fd4d2d192ae646ce9e4dd4d4c1e348e827caee
Mufaddal5253110 <mufaddalshakir55@gmail.com>
2023-05-14 18:51:02 +0530

    Product model and their actions added

---
+++ src/db/products.ts
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+const ProductSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    description: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    images: [
+        {
+            type: String,
+            required: true,
+        },
+    ],
+    quantity: {
+        type: Number,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    category: {
+        type: String,
+        required: true,
+    },
+});
+
+export const ProductModel = mongoose.model("Product", ProductSchema);
+
+// Product Actions
+export const getProducts = () => ProductModel.find();
+export const getProductsByCategory = (category: string) => ProductModel.find({category: category});
+export const searchProductsByName = (name: string) => ProductModel.find({name: { $regex: name, $options: "i" },});
+export const getProductById = (id: string) => ProductModel.findById(id);
+export const deleteProductById = (id: string) => ProductModel.findByIdAndDelete(id);
